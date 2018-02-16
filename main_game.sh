00be5581bf2eaec483aa17ac51a26777dbd286ea
Quentin90 <quentinoternaud@gmail.com>
2018-02-16 08:28:30 +0100

    bg_menu.txt

---
+++ main_game.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+############################################
+#                                          #
+#                                          #
+#              A SHELL RPG GAME            #
+#                                          #
+#                    2018                  #
+#                                          #
+############################################
+
+# Author : Quentin OTERNAUD , Quentin GANDILLET
+# Version : 0.01 Closed Beta
+
+# This is a one path Rogue like RPG based on the "In real life" RPG
+ cat bg_menu.txt
