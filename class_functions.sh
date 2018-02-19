5aee43aed23f054f039147aee3d12bf061893826
Quentin90 <quentinoternaud@gmail.com>
2018-02-19 15:25:20 +0000

    Update class_functions.sh

--- class_functions.sh
+++ class_functions.sh
@@ -1,19 +1,32 @@
 init_class(){
-	echo $1
-	if [ $1 -eq "0" ]
+    touch stats.txt	
+	> stats.txt
+	if [ $1 -eq "1" ]
 		then
 		echo "You chose Warrior"
-
-	elif [ $1 -eq "1" ]
-		then
-		echo "You chose Thief"
+		echo "str:5">>stats.txt
+		echo "mana:2">>stats.txt
+		echo "hp:15">>stats.txt
 
 	elif [ $1 -eq "2" ]
 		then
-		echo "You chose Mage"
+		echo "You chose Thief"
+		echo "str:5">>stats.txt
+		echo "mana:2">>stats.txt
+		echo "hp:15">>stats.txt
 
 	elif [ $1 -eq "3" ]
 		then
+		echo "You chose Mage"
+		echo "str:5">>stats.txt
+		echo "mana:2">>stats.txt
+		echo "hp:15">>stats.txt
+
+	elif [ $1 -eq "4" ]
+		then
 		echo "You chose Ranger"
+		echo "str:5">>stats.txt
+		echo "mana:2">>stats.txt
+		echo "hp:15">>stats.txt
 	fi
 }
